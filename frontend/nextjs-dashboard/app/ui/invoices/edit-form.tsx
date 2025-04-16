979a38bd2b796fe881203fc8b713d9205b512d8d
LeoObinna <leoaniude20@gmail.com>
2025-04-16 16:53:00 +0100

    Improving Accessibility

--- frontend/nextjs-dashboard/app/ui/invoices/edit-form.tsx
+++ frontend/nextjs-dashboard/app/ui/invoices/edit-form.tsx
@@ -1,16 +1,9 @@
 'use client';
 
 import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
-import {
-  CheckIcon,
-  ClockIcon,
-  CurrencyDollarIcon,
-  UserCircleIcon,
-} from '@heroicons/react/24/outline';
-import Link from 'next/link';
-import { Button } from '@/app/ui/button';
-import { updateInvoice } from '@/app/lib/actions';
-
+import { updateInvoice, State } from '@/app/lib/actions';
+import { useActionState } from 'react';
+ 
 export default function EditInvoiceForm({
   invoice,
   customers,
@@ -18,6 +11,9 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const initialState: State = { message: null, errors: {} };
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
-  return <form action={updateInvoiceWithId}>{/* ... */}</form>;
+  const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
+ 
+  return <form action={formAction}>{/* ... */}</form>;
 }
